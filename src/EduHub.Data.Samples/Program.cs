f2e223314b411d838d04803ae349af4ec0e9c24d
agacovski <alex.gacovski@outlook.com>
2026-06-25 14:46:15 +1000

    Restore EduHub.Data.Samples Program.cs file

--- src/EduHub.Data.Samples/Program.cs
+++ src/EduHub.Data.Samples/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Define the default EduHub Directory if non-standard environment (ie. testing)
-            EduHubContext.DefaultEduHubDirectory = @"C:\eduHub";
+            // EduHubContext.DefaultEduHubDirectory = @"E:\eduHub";
 
             // Simple Sample
             SimpleSample.Run();
@@ -14,10 +14,7 @@
             NavigationSample.Run();
 
             // Complex Sample (May only run successfully when AKC and AR datasets are manually added to Service C)
-           // ComplexSample.Run();
-
-            // Write-Back Sample
-            // WriteBackSample.Run();
+            ComplexSample.Run();
         }
     }
 }
